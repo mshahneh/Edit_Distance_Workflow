92616568610349bc20b7f4c78546c63d2252e421
mshahneh <mzare008@ucr.edu>
2024-12-10 10:45:11 -0800

    motif based edit distance

---
+++ motif_base_edit_distance.py
@@ -0,0 +1,111 @@
+def get_edges_index(mol, match):
+    """for a mol and a match, returns what edges are in the match
+    
+    Parameters
+    ----------
+    mol : rdkit.Chem.rdchem.Mol
+        the molecule
+    match : list
+        list of index of the atoms that are in the match
+    
+    """
+    binary_edge = 0
+    for i in range(len(match)):
+        for j in range(i+1, len(match)):
+            bond = mol.GetBondBetweenAtoms(match[i], match[j])
+            if bond is not None:
+                binary_edge += 1<<bond.GetIdx()
+    return binary_edge
+
+
+def get_sections(dp, par, final):
+    item = final
+    atom_group = dict()
+    index = 0
+    while par[item][0] is not None:
+        for atom in par[item][0][0]:
+            atom_group[atom] = index
+        index += 1
+        item = par[item][1]
+    return atom_group
+    
+
+def bfs(all_motif_matches, final, top_limit=None):
+    dp = dict()
+    par = dict()
+    forward_dp = dict()
+    forward_par = dict()
+    
+    for motif_cover in all_motif_matches:
+        move_back = final - motif_cover
+        forward_dp[move_back] = 1
+        forward_par[move_back] = (all_motif_matches[motif_cover], motif_cover)
+        
+    dp[0] = 0
+    par[0] = (None, None)
+
+    just_updated = set(dp.keys())
+    count = 1
+    while True:
+        new_just_updated = set()
+        for edge_status in just_updated:
+            for motif_cover in all_motif_matches:
+                if edge_status | motif_cover == edge_status + motif_cover:
+                    new_edge_status = edge_status + motif_cover
+                    if new_edge_status not in dp:
+                        dp[new_edge_status] = dp[edge_status] + 1
+                        par[new_edge_status] = (all_motif_matches[motif_cover], edge_status)
+                        new_just_updated.add(new_edge_status)
+                    elif dp[new_edge_status] > dp[edge_status] + 1:
+                        dp[new_edge_status] = dp[edge_status] + 1
+                        par[new_edge_status] = (all_motif_matches[motif_cover], edge_status)
+                        new_just_updated.add(new_edge_status)
+                
+                    if new_edge_status == final:
+                        break
+                    
+                    if new_edge_status in forward_dp:
+                        dp[final] = dp[new_edge_status] + forward_dp[new_edge_status]
+                        while new_edge_status != final:
+                            next_motif, next_edge = forward_par[new_edge_status]
+                            next_edge = new_edge_status + next_edge
+                            dp[next_edge] = dp[new_edge_status] + 1
+                            par[next_edge] = (next_motif, new_edge_status)
+                            new_edge_status = next_edge
+                        break
+                        
+            
+            if final in dp:
+                break
+            
+        if final in dp:
+            break
+        just_updated = new_just_updated.copy()
+        if len(just_updated) == 0:
+            break
+        
+        count += 1
+        if top_limit is not None and count >= top_limit:
+            break
+    
+    if final not in dp:
+        return top_limit + 1, None
+    else:
+        return dp[final], get_sections(dp, par, final)
+    
+
+def get_min_motif_to_cover(mol, motif_vocab, top_limit=None):
+    # first, create a dictionary of all the possible matches
+    all_motif_matches = dict()
+    for motif in motif_vocab:
+        matches = mol.GetSubstructMatches(motif)
+        for match in matches:
+            match_index = get_edges_index(mol, match)
+            if match_index not in all_motif_matches:
+                all_motif_matches[match_index] = (match, motif)
+    
+    # now, we need to find the minimum set of motifs that cover all the edges
+    final = 2**mol.GetNumBonds() - 1
+    res, sections = bfs(all_motif_matches, final, top_limit)
+    return res, sections
+    
--- pairs.py
+++ pairs.py
@@ -1,91 +1,153 @@
 
 #from ipywidgets import interact, fixed, widgets
 import pandas as pd
-from rdkit import Chem, Geometry
+from rdkit import Chem
 from rdkit.Chem import AllChem
-from rdkit.Chem import Draw
 from rdkit.Chem import rdFMCS
-from rdkit.Chem import PandasTools
 import argparse
 from tqdm import tqdm
 from rdkit import DataStructs
-from rdkit.Chem.Fingerprints import FingerprintMols
 import os
-from modifinder.utilities import mol_utils as mu
+import mol_utils as mu
 from myopic_mces import MCES
+from motif_base_edit_distance import get_min_motif_to_cover
+import pickle
 
-def solve_pair(smiles1, smiles2):
-    if type(smiles1) == str:
+atom_num_threshold = 55
+upper_limit_edit_distance = 10
+
+
+def motif_edit_distance(mol, mcs_mol, motifs, top_limit=None):
+    # print('here and the limit is', top_limit)
+    modifications = mu.get_modification_graph(mol, mcs_mol)
+    # print("modifications", modifications)
+    distance = 0
+    for modification in modifications:
+        res, sections = get_min_motif_to_cover(modification[0], motifs, top_limit)
+        distance += max(res, len(modification[1]))
+        if distance > upper_limit_edit_distance:
+            return distance
+    # print("done with motif edit distance")
+    return distance
+        
+
+def solve_pair(smiles1, smiles2, motifs_dict, top_limit=None, important_columns=None):
+    if isinstance(smiles1, str):
         mol1 = Chem.MolFromSmiles(smiles1)
     else:
         mol1 = smiles1
     
-    if type(smiles2) == str:
+    if isinstance(smiles2, str):
         mol2 = Chem.MolFromSmiles(smiles2)
     else:
         mol2 = smiles2
     
-    if mol1.GetNumAtoms() > 50 or mol2.GetNumAtoms() > 50:
+    if mol1.GetNumAtoms() > atom_num_threshold or mol2.GetNumAtoms() > atom_num_threshold:
         raise ValueError("The molecules are too large.")
     
-    fpgen = AllChem.GetRDKitFPGenerator(maxPath=3,fpSize=512)
-    fps = [fpgen.GetFingerprint(x) for x in [mol1, mol2]]
+    # print("going to calculate tanimoto")
+    fps = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, nBits=1024) for mol in [mol1, mol2]]
     tanimoto = DataStructs.TanimotoSimilarity(fps[0],fps[1])
-    if tanimoto < 0.2:
+    if tanimoto < 0.1:
         raise ValueError("The Tanimoto similarity is too low.")
+    
+    # print("tanimoto", tanimoto)
+    # print("going to calculate mcs")
     
     mcs1 = rdFMCS.FindMCS([mol1, mol2])
     mcs_mol = Chem.MolFromSmarts(mcs1.smartsString)
-    if mcs_mol.GetNumAtoms() < mol1.GetNumAtoms()//2 and mcs_mol.GetNumAtoms() < mol2.GetNumAtoms()//2:
+    
+    # print("mcs", mcs_mol.GetNumAtoms())
+    if mcs_mol.GetNumAtoms() < mol1.GetNumAtoms()//2 or mcs_mol.GetNumAtoms() < mol2.GetNumAtoms()//2:
         raise ValueError("The MCS is too small.")
     if mcs_mol.GetNumAtoms() <= 2:
         raise ValueError("The MCS is too small.")
     
-    dist1, dist2 = mu.get_edit_distance_detailed(mol1, mol2)
-
-    # if type(smiles1) != str:
-    #     smiles1 = Chem.MolToSmiles(mol1)
-    # if type(smiles2) != str:
-    #     smiles2 = Chem.MolToSmiles(mol2)
-    # mces_value = MCES(smiles1, smiles2)
-    distance = len(dist1) + len(dist2)
-    is_sub = ((len(dist1) == 0) or (len(dist2) == 0))
+    # print("going to calculate edit distance")
+    dist1, dist2 = mu.get_edit_distance_detailed(mol1, mol2, mcs_mol)
+    
+    distance = dist1 + dist2
+    is_sub = ((dist1 == 0) or (dist2 == 0))
+    # print("distance", distance)
+    
 
     res = dict()
     res['distance'] = distance
     res['tanimoto'] = tanimoto
     res['is_sub'] = is_sub
-    # res['mces'] = round(mces_value[1], 2)
-    # res['added'] = len(dist1)
-    # res['removed'] = len(dist2)
+    res['added'] = dist1
+    res['removed'] = dist2
+    
+    if 'mces' in important_columns:
+        if not isinstance(smiles1, str):
+            smiles1 = Chem.MolToSmiles(mol1)
+        if not isinstance(smiles2, str):
+            smiles2 = Chem.MolToSmiles(mol2)
+        mces_value = MCES(smiles1, smiles2, threshold=upper_limit_edit_distance)
+        res['mces'] = round(mces_value[1], 2)
+    
+    if "motif_distance" in important_columns:
+        # print("going to calculate motif edit distance")
+        if distance >= upper_limit_edit_distance:
+            motif_removed = dist2
+            motif_added = dist1
+        else:
+            # print("not passed the threshold")
+            motif_removed = motif_edit_distance(mol1, mcs_mol, motifs_dict, top_limit)
+            motif_added = motif_edit_distance(mol2, mcs_mol, motifs_dict, top_limit)
+        res['motif_distance'] = min(upper_limit_edit_distance, max(distance, motif_removed + motif_added))
+        res['motif_removed'] = motif_removed
+        res['motif_added'] = motif_added
+    
+    res['distance'] = min(upper_limit_edit_distance, distance)
+    
     return res
 
-def main(data_x, out_dir, data_y, verbose=True):
-    mols = {i: Chem.MolFromSmiles(row['Smiles']) for i, row in data_y.iterrows()}
+def main(cached_structures, cached_motifs, top_limit, data_x, out_dir, data_y, verbose=True):
+    important_columns = ['distance','tanimoto','is_sub','added','removed','mces','motif_distance','motif_removed','motif_added']
 
     with open(out_dir, 'w') as f:
-        f.write('distance,tanimoto,is_sub,inchi1,inchi2\n')
+        # f.write('distance,tanimoto,is_sub,inchi1,inchi2\n')
+        f.write("inchi1,inchi2," + ','.join(important_columns) + '\n')
         for i, row1 in tqdm(data_x.iterrows(), total=len(data_x), ascii=True, disable=not verbose):
-            moli = Chem.MolFromSmiles(row1['Smiles'])
-            if moli.GetNumAtoms() > 50:
+            ###### DEBUGGING ######
+            # if i != 76:
+            #     continue
+            
+            # print(i)
+            moli = cached_structures[row1['INCHI']]
+            if moli.GetNumAtoms() > atom_num_threshold:
                 continue
             for j, row2 in data_y.iterrows():
+                ###### DEBUGGING ######
+                # if j != 1348:
+                #     continue
                 
+                # print(row1['Smiles'], row2['Smiles'])
+                
+                molj = cached_structures[row2['INCHI']]
+                if molj.GetNumAtoms() > atom_num_threshold:
+                    continue
                 # warning!!! this is to make sure the process is only called for the upper triangular matrix, this assumes
                 # that the data is not reindexed and the data_x and data_y are just slices of the original dataframe
                 if i > j:
                     continue
                 try:
-                    res = solve_pair(moli, mols[j])
-                    distance = res['distance']
-                    tanimoto = res['tanimoto']
-                    is_sub = res['is_sub']
-                    # mces = res['mces']
-                    f.write(f"{distance},{tanimoto},{is_sub},\"{row1['INCHI']}\",\"{row2['INCHI']}\"\n")
+                    res = solve_pair(moli, molj, cached_motifs, top_limit, important_columns)
+                    str_res = [str(res[col]) for col in important_columns]
+                    f.write(f"\"{row1['INCHI']}\",\"{row2['INCHI']}\",")
+                    f.write(','.join(str_res) + '\n')
+                    # f.write(f"{distance},{tanimoto},{is_sub},\"{row1['INCHI']}\",\"{row2['INCHI']}\"\n")
+                    # f.write(f"{distance},{tanimoto},{is_sub},\"{row1['INCHI']}\",\"{row2['INCHI']}\",{mces},{res['added']},{res['removed']}\n")
                     # print("here")
                 except Exception as e:
-                    # print(e)
+                    # f.write("Error: " + str(e) + '\n')
+                    if 'int' in e.args[0] or 'float' in e.args[0]:
+                        raise e
                     continue
+            
+            f.flush()
+        
         
         # count += 1
         # # if 10 percent of progress is made, save the file
@@ -111,17 +173,22 @@ def get_data_index(data, index, batch_count):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Calculate edit distance and Tanimoto similarity between pairs of molecules.')
     parser.add_argument('data', type=str, help='Path to the input data file.')
+    parser.add_argument('cached_structures', type=str, help='Path to the cached structures file.')
+    parser.add_argument('motifs', type=str, help='Path to the motifs file.')
     parser.add_argument('out_dir', type=str, help='Path to the output directory.')
     parser.add_argument('--batch_x', type=int, help='Number of rows in the batch.', default=1)
     parser.add_argument('--batch_y', type=int, help='Number of columns in the batch.', default=1)
     parser.add_argument('--batch_index', type=int, help='Index of the batch to process.', default=0)
     parser.add_argument('--cached_dir', type=str, help='Path to the directory containing cached data.', default=None)
+    parser.add_argument('--top_limit', type=int, help='limit for depth of motif edit distance', default=4)
     parser.add_argument('--verbose', action='store_true', help='Print verbose output.')
     args = parser.parse_args()
 
     # if output dir is relative, make it absolute
     if not os.path.isabs(args.out_dir):
         args.out_dir = os.path.join(os.getcwd(), args.out_dir)
+        
+    print(args.out_dir)
     
     if not os.path.exists(os.path.dirname(args.out_dir)):
         os.makedirs(os.path.dirname(args.out_dir))
@@ -145,6 +212,12 @@ if __name__ == '__main__':
         if args.verbose:    
             print("No cache directory provided.")
     
+    with open(args.cached_structures, 'rb') as f:
+        cached_structures = pickle.load(f)
+        
+    with open(args.motifs, 'rb') as f:
+        motifs_dict = pickle.load(f)
+    
 
     data = pd.read_csv(args.data)
     batch_x = args.batch_x
@@ -165,6 +238,7 @@ if __name__ == '__main__':
     data_x = data.iloc[data_start:data_end]
     del data
 
-    main(data_x, out_dir, data_y, args.verbose)
-
+    if args.verbose:
+        print("done reading data", data_x.shape, data_y.shape)
+    main(cached_structures, motifs_dict, args.top_limit, data_x, out_dir, data_y, args.verbose)
 
