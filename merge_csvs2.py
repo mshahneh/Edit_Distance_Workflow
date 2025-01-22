f32ae4b93f359be3812e8dee6e37964e4c0835a3
mshahneh <mzare008@ucr.edu>
2025-01-22 11:28:23 -0800

    more general

--- merge_csvs2.py
+++ merge_csvs2.py
@@ -5,27 +5,28 @@ from tqdm import tqdm
 import pandas as pd
 
 # Set the directory containing your CSV files
-directory = '/home/user/LabData/Reza/data/Wout/120924/nf_output/res'
+directory = '/home/user/LabData/Reza/data/Wout/121024/res/'
+# directory = '/home/user/LabData/Reza/data/Wout/120924/nf_output/res/'
 
 # Name of the output file
-output_file = '/home/user/LabData/Reza/data/Wout/120924/nf_output/combined2.csv'
+output_file = '/home/user/LabData/Reza/data/Wout/121024/combined.csv'
+# output_file = '/home/user/LabData/Reza/data/Wout/121024/res/combined_old.csv'
 
 # Get a list of all CSV files in the directory
 all_files = glob.glob(os.path.join(directory, "*.csv"))
 
 # Process the first file separately to get the header
 first_file = all_files[0]
+df = pd.read_csv(first_file)
 
-
-# Write the header to the output file
-columns = ["inchi1","inchi2","distance","tanimoto","is_sub","added","removed","mces","motif_distance","motif_removed","motif_added"]
+columns = df.columns
 df = pd.DataFrame(columns=columns)
 df.to_csv(output_file, index=False)
-
+# header = True
 # Process the rest of the files 
 for file in tqdm(all_files):
-    # read csv file that doesnt have header
-    df = pd.read_csv(file, header=None)
+    df = pd.read_csv(file)
     df.to_csv(output_file, mode='a', header=False, index=False)
+    # header = False
 
 print(f"Merged {len(all_files)} CSV files into {output_file}")
